505b5c58c479325458196e4ecc9cbcd50e88da0b
akhilejoshy <akhilejoshy@gmail.com>
2024-09-24 16:14:01 +0530

    Update string_methods.py

--- data types and methods/string_methods.py
+++ data types and methods/string_methods.py
@@ -1,28 +1,28 @@
 a=''
 b=str()
-#the both use for emppty string
+##---> the both use for emppty string
 
 ##---> string formation
-# name='akhil'
-# designation='python django trainer'
-# company='luminar'
-# #im... i iam working as a... at ...
-# print('I am {0} i am working as a {1} at {2}'.format(name,designation,company))
-#
-# name='Akhil Joshy'
-# age=22
-# course='Python'
-# place='Thrissur'
-# qulification='B.Tech'
-# print('My name is {0:s} ,Iam from {1:s} ,my age is {2:d} ,and I complited {3:s} and im studing {4:s}'.format(name,place,age,qulification,course))
-# print('my name is %s ,I am from %s my age is %d ,and I complited %s and Im studing %s'%(name,place,age,qulification,course))
+name='akhil'
+designation='python django trainer'
+company='luminar'
+##---> im... i iam working as a... at ...
+print('I am {0} i am working as a {1} at {2}'.format(name,designation,company))
 
-# a=input('enter a strint:').upper()
-# print(a)
-# print(a.lower())
+name='Akhil Joshy'
+age=22
+course='Python'
+place='Thrissur'
+qulification='B.Tech'
+print('My name is {0:s} ,Iam from {1:s} ,my age is {2:d} ,and I complited {3:s} and im studing {4:s}'.format(name,place,age,qulification,course))
+print('my name is %s ,I am from %s my age is %d ,and I complited %s and Im studing %s'%(name,place,age,qulification,course))
 
-# b='john doe'
-# print(b[0].upper()+b[1:])
+a=input('enter a strint:').upper()
+print(a)
+print(a.lower())
+
+b='john doe'
+print(b[0].upper()+b[1:])
 
 c='abcdefghij'
 l=len(c)
