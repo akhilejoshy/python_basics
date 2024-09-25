932b18685e7310c56bb1c483793f8dce9ba8cffc
akhilejoshy <akhilejoshy@gmail.com>
2024-09-25 21:42:37 +0530

    .

--- code/practis/data types.py
+++ code/practis/data types.py
@@ -1,24 +1,24 @@
-##----->string
-# a="hello"
-# print(a)
-# print(type(a))
+##---> string
+a="hello"
+print(a)
+print(type(a))
 
-##----->list
-# l=['a','s','s','d']
-# print(l)
-# print(type(l))
+##---> list
+l=['a','s','s','d']
+print(l)
+print(type(l))
 
-##----->tuple
-# t=('q','f','d')
-# print(t)
-# print(type(t))
+##---> tuple
+t=('q','f','d')
+print(t)
+print(type(t))
 
-##----->dictionary
-# d={'a':1,'b':2}
-# print(d)
-# print(type(d))
+##---> dictionary
+d={'a':1,'b':2}
+print(d)
+print(type(d))
 
-##----->set
-# s={'a','v','q','d'}
-# print(s)
-# print(type(s))
+##---> set
+s={'a','v','q','d'}
+print(s)
+print(type(s))
--- code/practis/format.py
+++ code/practis/format.py
@@ -1,25 +1,23 @@
-##----->%s = string
-# str='welcome'
-# print('hello',str)
-# print('hello %s'%str)
+##---> %s = string
+str='welcome'
+print('hello',str)
+print('hello %s'%str)
 
-##----->to scan to two variables
-# company_name= 'luminar'
-# location='kakkand'
-# print('%s located in %s'%(company_name,location))
+##---> to scan to two variables
+company_name= 'luminar'
+location='kakkand'
+print('%s located in %s'%(company_name,location))
 
-##----->%d = number
-# name='abishek'
-# age=22
-# print('my name is %s and my age is %d'%(name,age))
+##---> %d = number
+name='abishek'
+age=22
+print('my name is %s and my age is %d'%(name,age))
 
-##----->%F = float number
-# #%2f for only two zeros
-# salary=15000
-# print('my salary is %.2f'%salary)
+##---> %F = float number
+salary=15000
+print('my salary is %.2f'%salary)
 
-##----->f formatters
-##----->use f infront of the word to print and use {} to scan
-# name='alex'
-# age=22
-# print(f'my name is {name} and my age is {age}')
+##---> f formatters
+name='alex'
+age=22
+print(f'my name is {name} and my age is {age}')
